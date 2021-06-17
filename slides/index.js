e0e12b187976fca593b80bd280f3ad2cd537b451
weiseng18 <tws.hci@gmail.com>
2021-06-17 20:19:09 +0800

    feat: show example of custom animation

--- slides/index.js
+++ slides/index.js
@@ -4,12 +4,36 @@ import { MText } from '../components/MotionChakra'
 import Slide from '../components/Slide'
 
 import { toggleOpacity } from '../animations'
+import { useEffect, useState } from 'react'
 
 const Title = ({ direction, step }) => {
+
+  const [color, setColor] = useState("black")
+  const [fontSize, setFontSize] = useState("16px")
+
+  useEffect(() => {
+    if (step >= 1) {
+      setColor("blue")
+      setFontSize("32px")
+    }
+    else {
+      setColor("black")
+      setFontSize("16px")
+    }
+  }, [step])
+
+
   return (
     <Slide direction={direction}>
       <VStack spacing={8}>
-        <MText>Hello World</MText>
+        <MText
+          animate={{
+            color: color,
+            fontSize: fontSize
+          }}
+        >
+          Hello World
+        </MText>
         {step === 1 && (
           <MText color="red"
             initial={toggleOpacity.initial}
