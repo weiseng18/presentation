8a7c2d23b6e0f1d09c54f9ea5cd7975a4439d274
weiseng18 <tws.hci@gmail.com>
2021-06-17 17:18:37 +0800

    feat: forward and backward

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,22 @@ import slides from '../slides'
 const Index = () => {
   const [slide, setSlide] = useState(0)
 
+  const changeSlide = (delta) => {
+    const newSlide = slide + delta
+    if (newSlide < 0 || newSlide >= slides.length)
+      return
+
+    setSlide(newSlide)
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'd') { changeSlide(1) }
+    else if (e.key === 'a') { changeSlide(-1) }
+  }
+
   return (
     <AnimatePresence initial={false}>
-      <Flex h="100vh" w="100vw" overflow="hidden" onClick={() => setSlide(slide === 0 ? 1 : 0)}>
+      <Flex h="100vh" w="100vw" overflow="hidden" tabIndex={0} onKeyDown={handleKeyDown}>
         {slides[slide]}
       </Flex>
     </AnimatePresence>
