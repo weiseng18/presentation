23a2b66555df59fade9470c2a32964cbeeb516e6
weiseng18 <tws.hci@gmail.com>
2021-06-18 10:41:37 +0800

    feat: add component TitleSlide

---
+++ components/TitleSlide.js
@@ -0,0 +1,18 @@
+import { Text, VStack } from '@chakra-ui/react'
+
+import React from 'react'
+
+import Slide from './Slide'
+
+const TitleSlide = ({ direction, title, subtitle }) => {
+  return (
+    <Slide direction={direction}>
+      <VStack spacing="60px">
+        <Text textStyle="heading1">{title}</Text>
+        <Text textStyle="body1">{subtitle}</Text>
+      </VStack>
+    </Slide>
+  )
+}
+
+export default TitleSlide
