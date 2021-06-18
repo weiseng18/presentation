57cd138b7318f3abd922820c7980e14fcafdadd1
weiseng18 <tws.hci@gmail.com>
2021-06-18 10:38:59 +0800

    feat: add textStyles

--- theme/index.js
+++ theme/index.js
@@ -1,9 +1,11 @@
 import { extendTheme } from '@chakra-ui/react'
 
 import { fonts } from './fonts'
+import { textStyles } from './textStyles'
 
 const theme = extendTheme({
-  fonts
+  fonts,
+  textStyles
 })
 
 export default theme
