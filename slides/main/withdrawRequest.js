018ece68cfb734d7efb249f43280c520cbee45b1
weiseng18 <tws.hci@gmail.com>
2021-06-22 15:18:46 +0800

    feat: add icons to slide 8

--- slides/main/withdrawRequest.js
+++ slides/main/withdrawRequest.js
@@ -1,5 +1,5 @@
 // base components
-import { HStack, List, ListIcon, Text, UnorderedList } from "@chakra-ui/layout"
+import { HStack, List, ListIcon, Text } from "@chakra-ui/layout"
 import { MListItem } from "../../components/MotionChakra"
 
 // slide components
@@ -8,6 +8,7 @@ import ContentSlide from "../../components/ContentSlide"
 // icons
 import { BsCheckCircle, BsPencilSquare } from "react-icons/bs"
 import { FaStamp } from "react-icons/fa"
+import { FcCancel, FcUndo } from "react-icons/fc"
 
 import { toggleOpacity } from "../../animations"
 
@@ -55,22 +56,28 @@ const Title = ({ direction, step }) => {
 const WhyImplement = ({ direction, step }) => {
   return (
     <ContentSlide direction={direction} title="Why implement?">
-      <UnorderedList spacing={8}>
+      <List spacing={8}>
         {step >= 1 && (
           <MListItem {...toggleOpacity} custom="enter">
-            <Text textStyle="body1">
-              Undo functionality for internal locus of control
-            </Text>
+            <HStack spacing={4}>
+              <ListIcon as={FcUndo} fontSize="24px" />
+              <Text textStyle="body1">
+                Undo functionality for internal locus of control
+              </Text>
+            </HStack>
           </MListItem>
         )}
         {step >= 2 && (
           <MListItem {...toggleOpacity} custom="enter">
-            <Text textStyle="body1">
-              Users had to ask their admin to ignore incorrect requests
-            </Text>
+            <HStack spacing={4}>
+              <ListIcon as={FcCancel} fontSize="24px" />
+              <Text textStyle="body1">
+                Users had to ask their admin to ignore incorrect requests
+              </Text>
+            </HStack>
           </MListItem>
         )}
-      </UnorderedList>
+      </List>
     </ContentSlide>
   )
 }
