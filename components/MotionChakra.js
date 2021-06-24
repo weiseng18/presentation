6da4998c4f7eaf4100675ff882b22ab67a51ebd3
weiseng18 <tws.hci@gmail.com>
2021-06-24 14:41:40 +0800

    feat: add motion variant for list

--- components/MotionChakra.js
+++ components/MotionChakra.js
@@ -1,13 +1,22 @@
 // base components
-import { Flex, HStack, Image, ListItem, Text, VStack } from "@chakra-ui/react"
+import {
+  Flex,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Text,
+  VStack,
+} from "@chakra-ui/react"
 
 import { motion } from "framer-motion"
 
 const MFlex = motion(Flex)
 const MHStack = motion(HStack)
 const MImage = motion(Image)
+const MList = motion(List)
 const MListItem = motion(ListItem)
 const MText = motion(Text)
 const MVStack = motion(VStack)
 
-export { MFlex, MHStack, MImage, MListItem, MText, MVStack }
+export { MFlex, MHStack, MImage, MList, MListItem, MText, MVStack }
