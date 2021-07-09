34358af7d80e5e487e661aae034202f6dec0eb7b
weiseng18 <tws.hci@gmail.com>
2021-07-09 15:12:12 +0800

    refactor: make DatasetTable its own component

--- slides/main/refactorDatasets.js
+++ slides/main/refactorDatasets.js
@@ -1,24 +1,7 @@
 // base components
-import {
-  Grid,
-  List,
-  Text,
-  Tbody,
-  Th,
-  Thead,
-  Tr,
-  UnorderedList,
-  VStack,
-} from "@chakra-ui/react"
+import { Grid, List, Text, UnorderedList, VStack } from "@chakra-ui/react"
 
-import {
-  MFlex,
-  MListItem,
-  MTable,
-  MTd,
-  MText,
-  MTr,
-} from "../../components/MotionChakra"
+import { MFlex, MListItem, MTd, MText } from "../../components/MotionChakra"
 
 // slide components
 import ContentSlide from "../../components/ContentSlide"
@@ -28,7 +11,7 @@ import SyntaxHighlighter from "react-syntax-highlighter"
 import { vs2015 } from "react-syntax-highlighter/dist/cjs/styles/hljs"
 
 import { toggleOpacity } from "../../animations"
-import { useEffect, useState } from "react"
+import DatasetTable from "../../components/custom/DatasetTable"
 
 const Scope = ({ direction, step }) => {
   return (
@@ -169,132 +152,9 @@ const Cell = ({ t, animate }) => {
 }
 
 const DatasetFlows = ({ direction, step }) => {
-  const [opacity, setOpacity] = useState([1, 1, 1, 1, 1])
-
-  useEffect(() => {
-    document.addEventListener("keypress", (e) => {
-      // e.code: DigitX
-      const code = e.code
-      if (code.length === 6 && code[5] == parseInt(code[5]))
-        toggleCol(parseInt(code[5]), e.shiftKey)
-    })
-  }, [])
-
-  /**
-   *
-   * @param {*} which digit 1-5, or 0
-   * @param {*} shiftKey whether shiftKey was pressed or not
-   */
-  const toggleCol = (which, shiftKey) => {
-    let arr
-    if (which === 0) {
-      arr = [1, 1, 1, 1, 1]
-    }
-    // if shift key was pressed, then add on, rather than toggle.
-    else if (shiftKey) {
-      // currently does not work
-      // some strange react state thing
-      arr = opacity
-      arr[which - 1] = 1
-    }
-    // else, turn off everything else, and turn on current col
-    else {
-      arr = [0.1, 0.1, 0.1, 0.1, 0.1]
-      arr[which - 1] = 1
-    }
-    setOpacity(arr)
-  }
-
   return (
     <ContentSlide direction={direction} title="Dataset flows">
-      {step >= 1 && (
-        <MTable {...toggleOpacity} custom="enter">
-          <Thead>
-            <Tr>
-              <Th w="25%" fontSize="sm">
-                Flow
-              </Th>
-              <Th fontSize="sm" animate={{ opacity: opacity[0] }}>
-                Ingested
-              </Th>
-              <Th fontSize="sm" animate={{ opacity: opacity[1] }}>
-                Has Dataset
-              </Th>
-              <Th fontSize="sm" animate={{ opacity: opacity[2] }}>
-                Has Sample
-              </Th>
-              <Th fontSize="sm" animate={{ opacity: opacity[3] }}>
-                Needs Approval
-              </Th>
-              <Th w="15%" fontSize="sm" animate={{ opacity: opacity[4] }}>
-                Categories
-              </Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Cell t="1 (Regular)" />
-              <Cell animate={{ opacity: opacity[0] }} t="Y" />
-              <Cell animate={{ opacity: opacity[1] }} t="Y" />
-              <Cell animate={{ opacity: opacity[2] }} t="Y" />
-              <Cell animate={{ opacity: opacity[3] }} t="Y" />
-              <Cell animate={{ opacity: opacity[4] }} t="Not Geospatial" />
-            </Tr>
-            <Tr>
-              <Cell t="2 (Confidential)" />
-              <Cell animate={{ opacity: opacity[0] }} t="Y" />
-              <Cell animate={{ opacity: opacity[1] }} t="Y" />
-              <Cell animate={{ opacity: opacity[2] }} t="Y" />
-              <Cell animate={{ opacity: opacity[3] }} t="Y" />
-              <Cell animate={{ opacity: opacity[4] }} t="Not Geospatial" />
-            </Tr>
-            <Tr>
-              <Cell t="3 (Metadata-Only)" />
-              <Cell animate={{ opacity: opacity[0] }} t="N" />
-              <Cell animate={{ opacity: opacity[1] }} t="N" />
-              <Cell animate={{ opacity: opacity[2] }} t="N" />
-              <Cell animate={{ opacity: opacity[3] }} t="Y" />
-              <Cell animate={{ opacity: opacity[4] }} t="Any" />
-            </Tr>
-            {step >= 2 && (
-              <MTr {...toggleOpacity} custom="enter">
-                <Cell t="4 (Pre-Approved, non-CSV)" />
-                <Cell animate={{ opacity: opacity[0] }} t="N" />
-                <Cell animate={{ opacity: opacity[1] }} t="Y" />
-                <Cell animate={{ opacity: opacity[2] }} t="N" />
-                <Cell animate={{ opacity: opacity[3] }} t="N" />
-                <Cell
-                  animate={{ opacity: opacity[4] }}
-                  t="Geospatial, Others"
-                />
-              </MTr>
-            )}
-            {step >= 3 && (
-              <MTr {...toggleOpacity} custom="enter">
-                <Cell t="5 (Pre-Approved, CSV)" />
-                <Cell animate={{ opacity: opacity[0] }} t="Y" />
-                <Cell animate={{ opacity: opacity[1] }} t="Y" />
-                <Cell animate={{ opacity: opacity[2] }} t="Y" />
-                <Cell animate={{ opacity: opacity[3] }} t="N" />
-                <Cell
-                  animate={{ opacity: opacity[4] }}
-                  t="Not (Geospatial, Others)"
-                />
-              </MTr>
-            )}
-            {step >= 4 && (
-              <MTr {...toggleOpacity} custom="enter">
-                <Cell t="6 (Requires approval, non-CSV)" />
-                <Cell animate={{ opacity: opacity[0] }} t="N" />
-                <Cell animate={{ opacity: opacity[1] }} t="Y" />
-                <Cell animate={{ opacity: opacity[2] }} t="N" />
-                <Cell animate={{ opacity: opacity[3] }} t="Y" />
-                <Cell animate={{ opacity: opacity[4] }} t="??" />
-              </MTr>
-            )}
-          </Tbody>
-        </MTable>
-      )}
+      {step >= 1 && <DatasetTable step={step} />}
       {step === 5 && (
         <MFlex
           position="absolute"
